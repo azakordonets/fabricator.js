b8b3ee5c4a8a82ebe15964a527338bdd6cadc940
Andrii Zakordonets <andrii.zakordonets@portavita.nl>
2016-05-31 20:00:15 +0200

    added more methods for issue #8

--- src/alpha.js
+++ src/alpha.js
@@ -4,6 +4,18 @@ export default class Alphanumeric {
   }
 
   randomNumber(min = 0, max = 1000) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(getRandomNumber(min, max));
   }
+
+  randomFloat(min = 0, max = 1000, precision = 5) {
+    return (getRandomNumber(min, max)).toPrecision(precision);
+  }
+
+  listOfRandomNumbers(length = 100, min = 0, max = 1000) {
+    return new Array(length).fill(this.randomNumber(min, max));
+  }
+}
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min + 1) + min;
 }
--- test/alpha.test.js
+++ test/alpha.test.js
@@ -5,19 +5,26 @@ const alpha = new Fabricator().alpha();
 
 const isInt = number => Number(number) === number && number % 1 === 0;
 
+const isFloat = number => !!(number % 1);
+
+function isInRange(number, min, max) {
+  return number >= min && number <= max;
+}
+
+/**
+ * Int numbers tests
+ */
+
 test('generates random number from default range (0 to 1000)', t => {
   const number1 = alpha.randomNumber();
   const number2 = alpha.randomNumber();
   t.true(isInt(number1));
   t.true(isInt(number2));
-  t.true(number1 !== number2);
+  t.notDeepEqual(number1, number2);
   t.true(number1 >= 0 && number1 <= 1000);
   t.true(number2 >= 0 && number2 <= 1000);
 });
 
-function isInRange(number, min, max) {
-  return number >= min && number <= max;
-}
 
 function randomNumberWithCustomRange(t, min, max) {
   const number1 = alpha.randomNumber(min, max);
@@ -38,3 +45,45 @@ test('generated number is in range', randomNumberWithCustomRange, -300, -100);
 test('generated number is in range', randomNumberWithCustomRange, 0, 9007199254740992);
 test('generated number is in range',
     randomNumberWithCustomRange, -9007199254740992, 9007199254740992);
+
+
+/**
+ * Float number test
+ */
+
+test('generates random number with floating point in default range (0 to 1000)' +
+' with default precision = 5', t => {
+  const number1 = alpha.randomFloat();
+  const number2 = alpha.randomFloat();
+  t.true(isFloat(number1));
+  t.true(isFloat(number2));
+  t.notDeepEqual(number1, number2);
+  t.true(number1 >= 0 && number1 <= 1000);
+  t.true(number2 >= 0 && number2 <= 1000);
+});
+
+/**
+ * List generation tests
+ */
+
+test('generated list of random numbers', t => {
+  const listOfRandomNumbers = alpha.listOfRandomNumbers();
+  t.deepEqual(100, listOfRandomNumbers.length);
+  listOfRandomNumbers.forEach((number) => {
+    t.true(isInt(number));
+    t.true(number >= 0 && number <= 1000);
+  });
+});
+
+test('generated list of random numbers with custom range', t => {
+  const min = 300;
+  const max = 4000;
+  const listOfRandomNumbers = alpha.listOfRandomNumbers(100, min, max);
+  t.deepEqual(100, listOfRandomNumbers.length);
+  listOfRandomNumbers.forEach((number) => {
+    t.true(isInt(number));
+    t.true(number >= min && number <= max);
+  });
+});
+
+
