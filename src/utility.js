3b79ae441b8aca93cf752ae9c71d5ee5e880d9b6
Andrii Zakordonets <andrii.zakordonets@portavita.nl>
2016-06-09 16:28:06 +0200

    add utility class to read data from json files

--- src/utility.js
+++ src/utility.js
@@ -1,6 +1,24 @@
+import readFile from 'read-file-relative';
+import Alphanumeric from './alpha.js';
+import _ from 'lodash';
 export default class UtilityService {
 
-  constructor(lang = 'en') {
+  constructor(lang = 'us') {
     this.lang = lang;
+    this.resourceFileName = `/resources/${this.lang}.json`;
+    this.alpha = new Alphanumeric();
+    this.wordsFileName = `/resources/${this.lang}.json`;
   }
+
+  getValue(name) {
+    const valuesArray = this.getValuesArray(name);
+    return valuesArray[this.alpha.randomNumber(0, valuesArray.length - 1)];
+  }
+
+  getValuesArray(name) {
+    const data = readFile.readSync(this.resourceFileName);
+    const json = JSON.parse(data);
+    return _.at(json, name)[0];
+  }
+
 }
