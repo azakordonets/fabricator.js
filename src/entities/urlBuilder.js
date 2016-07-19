d5181ae6c5959df86befd6187ecdfc94762f7b86
Andrew Zakordonets <biercoff@gmail.com>
2016-07-19 11:43:09 +0200

    issue #30 add constructor to urlBuilder

--- src/entities/urlBuilder.js
+++ src/entities/urlBuilder.js
@@ -1,2 +1,9 @@
+import UtilityService from '../utility';
 export default class UrlBuilder {
+
+  constructor(lang = 'us') {
+    this.lang = lang;
+    this.util = new UtilityService(lang);
+  }
+
 }
